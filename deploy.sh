8a560dca37afe813b2178325ddf993d6bb06fb27
Hannah <ghdcoalss33@gmail.com>
2023-02-02 12:05:49 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,14 @@ yarn export
 git add out/
 git commit -m "deploy to gh-pages"
 git subtree push --prefix out origin gh-pages
+git checkout gh-pages
 
-rm -rf ../HongChaeMin.github.io/
-cp out/** ../HongChaeMin.github.to/**
-
+cd ..
+rm -rf HongChaeMin.github.io/**
+cp -rf blog-front/** HongChaeMin.github.io
+cd HongChaeMin.github.io
+git pull
+git add --all
+git commit -m "deploy"
+git push
+git checkout feat/resume
