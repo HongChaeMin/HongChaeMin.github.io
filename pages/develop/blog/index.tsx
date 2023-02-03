d09e94afd7a85f5016e4a8a48e4cdb331be7d139
Hannah <ghdcoalss33@gmail.com>
2023-02-03 10:48:57 +0900

    어영부영 급하게 만들기...

---
+++ pages/develop/blog/index.tsx
@@ -0,0 +1,22 @@
+import Post from "../../../components/main/Post";
+import { postData } from "../../../data/postData";
+import styled from "styled-components";
+import { TitleText } from "../../../components/styles/styleCompnents";
+
+export default function Blog() {
+  return <div className={"w-full"}>
+      <TitleText>Develop Blog Page</TitleText>
+      <BlogListBox>
+        { postData.map((post, idx) => {
+          return <Post key={idx} post={post} idx={idx} />;
+        })}
+      </BlogListBox>
+    </div>;
+}
+
+const BlogListBox = styled.div`
+  display: flex;
+  -webkit-flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`;
